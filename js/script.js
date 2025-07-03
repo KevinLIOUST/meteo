f05d59085d8868829f54ab5d557418f5938dc378
KevinLIOUST <kevin.lioust@outlook.fr>
2025-07-03 22:21:15 +0200

    Importation des icônes, création de l'interface pour la page de la météo avec les affichages quasi-finis ! :)

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 let city = "rouen";
 let apiKey = "379a99adac6672b321cbd6175e3c6efd";
 // let tabVilles = ["rouen", "caen", "paris"];
+let tabJoursSemaine = ["lundi", "mardi", "mercredi", "jeudi", "vendredi", "samedi", "dimanche"];
 
 let url = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&lang=fr&units=metric`;
 
@@ -29,9 +30,31 @@ function recupererDonneesMeteo() {
 
       document.getElementById("date").innerText = json.list[0].dt_txt;
 
-      document.getElementById("temperature").innerText = `${json.list[0].main.temp} °`;
+      document.getElementById(
+        "temperature"
+      ).innerText = `${json.list[0].main.temp} °`;
 
+      document.getElementById("infos-meteo-pression-atmosphérique").innerText =
+        json.list[0].main.pressure + " mmhg";
 
-    })
+      document.getElementById("infos-meteo-humidite").innerText =
+        json.list[0].main.humidity + " %";
+
+      document.getElementById("infos-meteo-vit-wind").innerText =
+        json.list[0].wind.speed + " km/h";
+
+        let j = 0;
+        for (let i=0; i<json.list.length; i+=8) {
+          document.getElementById("temperature-max-" + tabJoursSemaine[j]).innerText = json.list[i].main.temp_max;
+          document.getElementById("temperature-min-" + tabJoursSemaine[j]).innerText = json.list[i].main.temp_min;
+          j++;
+        }
+
+        let jour = 0;
+        for (let i = 0; i < json.list.length; i+=8) {
+          document.getElementById("date-" + tabJoursSemaine[jour]).innerText = json.list[i].dt_txt;
+          jour++;
+        }
+    });
 }
-recupererDonneesMeteo()
+recupererDonneesMeteo();
